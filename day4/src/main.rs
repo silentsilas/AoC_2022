d18617addade866d0ac57ba7efe88db544088661
Silas <me@silentsilas.com>
2022-12-05 01:01:00 -0500

    solve day 4 part 2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -6,30 +6,33 @@ fn main() {
     let file_path = &args[1];
     let contents = fs::read_to_string(file_path).unwrap();
     
-    let part_1_result = solve_part_one(&contents);
+    let (part_1_result, part_2_result) = solve(&contents);
     println!("{}", part_1_result);
-
-    let part_2_result = solve_part_two(&contents);
     println!("{}", part_2_result);
 }
 
-pub fn solve_part_one(contents: &str) -> i32 {
+pub fn solve(contents: &str) -> (i32, i32) {
     let pairs: Vec<&str> = contents.split('\n').collect();
-    let mut result = 0;
+    let mut part_1_result = 0;
+    let mut part_2_result: i32 = 0;
     for pair in pairs {
         if let [elf1, elf2] = pair.split(',').collect::<Vec<&str>>()[..] {
 
             let elf_range1 = parse_elf_rooms(elf1);
             let elf_range2 = parse_elf_rooms(elf2);
             if contains_range(elf_range1, elf_range2) || contains_range(elf_range2, elf_range1) {
-                result += 1;
+                part_1_result += 1;
+            }
+
+            if intersects_range(elf_range1, elf_range2){
+                part_2_result += 1;
             }
 
         } else {
             continue;
         }
     }
-    result
+    (part_1_result, part_2_result)
 }
 
 fn parse_elf_rooms(elf: &str) -> (i32, i32) {
@@ -48,9 +51,11 @@ fn contains_range(range1: (i32, i32), range2: (i32, i32)) -> bool {
     low1 <= low2 && high1 >= high2
 }
 
-pub fn solve_part_two(contents: &str) -> i32 {
+fn intersects_range(range1: (i32, i32), range2: (i32, i32)) -> bool {
+    let (low1, high1) = range1;
+    let (low2, high2) = range2;
 
-    10
+    low1 <= high2 && high1 >= low2
 }
 
 #[cfg(test)]
@@ -62,7 +67,8 @@ mod tests {
         let dir = env!("CARGO_MANIFEST_DIR").to_string();
         let path = dir + "/assets/test.txt";
         let file = fs::read_to_string(path).unwrap();
-        let result = solve_part_one(&file);
-        assert_eq!(result, 2);
+        let (first_result, second_result) = solve(&file);
+        assert_eq!(first_result, 2);
+        assert_eq!(second_result, 4);
     }
 }
